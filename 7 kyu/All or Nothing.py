88f22d2b3bf798e3f3589b173bebe8b070645ebf
Benedict Halim <benedicthalim1@gmail.com>
2024-03-11 16:18:06 +0700

    Completed 7 kyu - "All or Nothing"

--- 7 kyu/All or Nothing.py
+++ 7 kyu/All or Nothing.py
@@ -1,7 +1,18 @@
-
 def possibly_perfect(key, answers):
-    for i in len(key):
-        if key[i] == '':
-            return True
+    right = 0
+    wrong = 0
+    removedUnderScore = 0
+    for i in range(len(key)):
+        if key[i] != "_":
+            if key[i] == answers[i]:
+                right += 1
+            elif key[i] != answers[i]:
+                wrong += 1
         else:
-            return False
+            removedUnderScore += 1
+    if (right == (len(key) - removedUnderScore)) or (wrong == (len(key) - removedUnderScore)):
+        return True
+    else:
+        return False
+
+#In the future, Go make this code efficient
