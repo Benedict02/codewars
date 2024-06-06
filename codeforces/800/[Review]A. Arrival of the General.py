d738b5f7361fec94f213dbd0454fb456677617c6
Benedict Halim <benedicthalim1@gmail.com>
2024-06-06 20:19:19 +0700

    I found my priority in programming: make money

--- codeforces/800/[Review]A. Arrival of the General.py
+++ codeforces/800/[Review]A. Arrival of the General.py
@@ -7,7 +7,23 @@ def MII(): return list(map(int, sys.stdin.readline().replace('\n', '').split()))
 def MSI(): return list(sys.stdin.readline().replace('\n', ''))
 
 def ArrivalOfTheGeneral():
-    
+    n = II()
+    a = MII()
+    amax = max(a)
+    amin = min(a)
+    if amin == amax: return 0
+    for i in range(n):
+        if a[-(i+1)] == amax:
+            imax = n - (i+1)
+        if a[i] == amin:
+            imin = i
+    res = (imax - 0) + ((n-1)-imin)
+    if imin > imax:
+        return res
+    if imax > imin:
+        return res-1
+
+
 
 if __name__ == '__main__':
-    ArrivalOfTheGeneral()
+    O(ArrivalOfTheGeneral())
